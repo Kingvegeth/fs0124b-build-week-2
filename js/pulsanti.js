7e41262a617e1f35e2ebf832332b9de5e590b27c
SalvatoreMura <tore.291196@gmail.com>
2024-03-13 18:15:07 +0100

    czxc

--- js/pulsanti.js
+++ js/pulsanti.js
@@ -1,19 +1,19 @@
 
-            //pulsante che mostra e nasconde la lista amici
-            let friendsToggle = document.getElementById('friendlist-toggle');
-            let sectionRight = document.getElementById('section-right');
-            let sectionCenter = document.getElementById('center-page');
+//pulsante che mostra e nasconde la lista amici
+let friendsToggle = document.getElementById('friendlist-toggle');
+let sectionRight = document.getElementById('section-right');
+let sectionCenter = document.getElementById('center-page');
 
-            friendsToggle.addEventListener('click',function(){
+friendsToggle.addEventListener('click', function () {
 
-                    sectionRight.classList.remove('d-none')
-                    sectionCenter.classList.remove('col-lg-10')
-                    sectionCenter.classList.add('col-lg-8')
-            })
+        sectionRight.classList.remove('d-none')
+        sectionCenter.classList.remove('col-lg-10')
+        sectionCenter.classList.add('col-lg-8')
+})
 
-            let closeFriends = document.querySelector('.close-friends')
-            closeFriends.addEventListener('click', function(){
-                sectionRight.classList.add('d-none')
-                    sectionCenter.classList.remove('col-lg-8')
-                    sectionCenter.classList.add('col-lg-10')
-            })
+let closeFriends = document.querySelector('.close-friends')
+closeFriends.addEventListener('click', function () {
+        sectionRight.classList.add('d-none')
+        sectionCenter.classList.remove('col-lg-8')
+        sectionCenter.classList.add('col-lg-10')
+})
